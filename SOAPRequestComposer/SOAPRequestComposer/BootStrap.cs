1105dc23582da11f5f9b17506309fdf70814b0ad
ironpython2001 <ironpython2001@gmail.com>
2014-10-13 13:53:49 +0530

    BootStrap

--- SOAPRequestComposer/SOAPRequestComposer/BootStrap.cs
+++ SOAPRequestComposer/SOAPRequestComposer/BootStrap.cs
@@ -6,9 +6,25 @@ using Fiddler;
 
 namespace SOAPRequestComposer
 {
-    public class BootStrap
+    public class BootStrap : IHandleExecAction, IFiddlerExtension
     {
 
 
+        public bool OnExecAction(string sCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnBeforeUnload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnLoad()
+        {
+            throw new NotImplementedException();
+        }
+
+     
     }
 }
